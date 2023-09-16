621d6f299d045552e3c890ebcc62dd4925db4bb0
skxvtchy <jamesli784@gmail.com>
2023-09-16 19:14:11 -0400

    navbar

---
+++ guppii/navigation/screens/Messages.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+export default function Messages({ navigation }) {
+  return (
+    <View>
+      <Text style={{ fontSize: 50, fontWeight: "bold" }}>Messages</Text>
+    </View>
+  );
+}
