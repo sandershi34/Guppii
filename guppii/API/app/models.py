ebbe3e5f15d216395a61ddc28eb5bfc9263fdf14
joshtopher <jefriedl@buffalo.edu>
2023-09-17 15:50:33 -0400

    Login/signup

--- guppii/API/app/models.py
+++ guppii/API/app/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=16)
-    password = models.CharField(max_length=24)
+    password_hash = models.CharField(max_length=24)
+    user_token = models.CharField(max_length=64)
--- guppii/API/app/views.py
+++ guppii/API/app/views.py
@@ -6,6 +6,8 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from app.serializers import UserSerializer, GroupSerializer
 from app.models import User
+import bcrypt
+import secrets
 
 
 
@@ -32,17 +34,36 @@ class GroupViewSet(viewsets.ModelViewSet):
 def getData(request):
     return Response({'Hello': 'World'})
 
+
+@api_view(['POST'])
+def signup(request):
+    username = request.data.get('username')
+    if not username:
+        return Response({'Signup': 'No Username Given'})
+    alreadyExists = User.objects.filter(username=username).first()
+    if alreadyExists:
+        return Response({'Signup': 'Username Already Exists'})
+    password = request.data.get('password')
+    pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+    user = User(username=username, password_hash=pw_hash)
+    user.save()
+    return Response({'Signup': 'Success'})
+
+
 @api_view(['POST'])
 def login(request):
-    username = request.data.get('name')
+    username = request.data.get('username')
     if not username:
-        return Response({'Login': 'Fail'})
-    password = request.data.get('pass1')
-
-    user = User(username=username, password=password)
-    # user.save()
-    return Response({'Login': 'Success'})
-
+        return Response({'Login': 'No Username Given'})
+    password = request.data.get('password')
+    user = User.objects.filter(username=username).first()
+    if not user:
+        return Response({'Login': 'User Does Not Exist'})
+    if not bcrypt.checkpw(password.encode(), user.password_hash.encode()):
+        return Response({'Login': 'Username or Password Incorrect'})
+    user_token = secrets.token_urlsafe(64)
+    user.user_token = user_token
+    return Response({'Login': 'Success', 'token': user_token})
 
 
 
