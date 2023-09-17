23d3579e038ad233ce084a6563ffc6729435cce4
sandersh <sandershi34@gmail.com>
2023-09-17 11:20:02 -0400

    initiated app

--- guppii/API/API/urls.py
+++ guppii/API/API/urls.py
@@ -16,10 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from rest_framework import routers
+from app import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.getData),
+    ##path('', views.getData),
     path('', include('django.contrib.auth.urls')),
+
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
