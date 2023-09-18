0a606e50e2f43e89431db9429c1dee5618e82686
joshtopher <jefriedl@buffalo.edu>
2023-09-17 21:31:01 -0400

    Added paths

--- guppii/API/server.py
+++ guppii/API/server.py
@@ -2,9 +2,12 @@ import json
 import bcrypt
 import secrets
 from pymongo import MongoClient
-from bson import json_util
+# from bson import json_util
 from flask import Flask, request, redirect, Response ,jsonify
 
+client = MongoClient("mongo")
+DB = client['guppii']
+USERS = DB['users']
 
 app = Flask(__name__)
 
@@ -14,46 +17,28 @@ def getData():
     return json.dumps({"message": "Hello World!"})
 
 
-@app.post("/signup")
-def signup():
-    username = request.data.get('username')
-    if not username:
-        return json.dumps({"error": "username is required"})
-    # alreadyExists = User.objects.filter(username=username).first()
-    alreadyExists = False
-    if alreadyExists:
-        return json.dumps({"error": "username already exists"})
-    password = request.data.get('password')
-    pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
-    # user = User(username=username, password_hash=pw_hash)
-    # user.save()
-    return json.dumps({"message": "success"})
+@app.post("/new-user")
+def new_user():
+    name = request.form.get("Name")
+    skills = request.form.get("Skills")
+    preferences = request.form.get("Preferences")
+    USERS.insert_one({"Name": name, "Skills": skills, "Preferences": preferences})
+    return json.dumps({"message": "User created successfully"})
 
 
-@app.post("/login")
-def login():
-    username = request.data.get('username')
-    if not username:
-        return json.dumps({"error": "username is required"})
-    password = request.data.get('password')
-    # user = User.objects.filter(username=username).first()
-    user = True
-    pw_hash = ""
+@app.get("/user/<name>")
+def get_user(name):
+    user = USERS.find_one({"Name": name})
     if not user:
-        return json.dumps({"error": "username or password is incorrect"})
-    if not bcrypt.checkpw(password.encode(), pw_hash.encode()):
-        return json.dumps({"error": "username or password is incorrect"})
-    user_token = secrets.token_urlsafe(64)
-    user.user_token = user_token
-    return json.dumps({"message": "success", "token": user_token})
+        return json.dumps({"error": "User not found"})
+    return json.dumps({"Name": user["Name"], "Skills": user["Skills"], "Preferences": user["Preferences"]})
+
+
 
 @app.get("/users")
 def users():
-    client = MongoClient("mongodb+srv://guppii:guppii@guppii.6uahsyp.mongodb.net/")
-    db = client['guppii']
-    users = db['users']
-    user_info = list(users.find())
-    resp = json.loads(json_util.dumps(user_info))
+    user_info = list(USERS.find())
+    resp = json.loads(json.dumps(user_info))
     return resp
 
 @app.post("/user/")
